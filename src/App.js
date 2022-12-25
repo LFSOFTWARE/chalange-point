8da1d13e18410ef99329a33e527490ecae5c6644
LFSOFTWARE <luiz_fernando-br2015@hotmail.com>
2022-12-24 21:31:35 -0300

    feat:  voltar e refazar ponto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [points, setPoints] = useState([])
+  const [pointsCopy, setPointsCopy] = useState([])
+
+  const generatePoints = (event) => {
+    const { clientX, clientY } = event
+    setPoints((prev) => [...prev, { clientX, clientY }])
+    setPointsCopy((prev) => [...prev, { clientX, clientY }])
+  }
+
+  const blockPointOnTheButton = () => {
+    if (points.length === 0) return
+    setPoints((prev) => {
+      let arr = [...prev]
+      arr.slice(0, points.length - 1)
+      return arr
+    })
+  }
+
+  const undo = (event) => {
+    event.stopPropagation()
+    blockPointOnTheButton()
+
+    setPoints((prev) => {
+      let arr = [...prev]
+      arr.pop();
+      return arr
+    })
+  }
+
+  const redo = (event) => {
+    event.stopPropagation()
+    blockPointOnTheButton()
+    try {
+      
+      const lastPositon = points.at(-1)
+      const indexCopy = pointsCopy.findIndex((p) => p.clientX === lastPositon.clientX && p.clientY === lastPositon.clientY)
+  
+      setPoints((prev) => {
+        let arr = [...prev]
+        if (pointsCopy[indexCopy + 1] !== undefined) {
+          arr.push(pointsCopy[indexCopy + 1]);
+        }
+        return arr
+      })
+    } catch (error) {
+      setPoints((prev) => {
+        let arr = [...prev]
+          arr.push(pointsCopy[0]);
+        return arr
+      })
+    }
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" onClick={generatePoints}>
+      <div className='btns'>
+        <button onClick={undo}>Voltar</button>
+        <button onClick={redo}>Refazer</button>
+      </div>
+      {points.map((point) => (
+        <div key={JSON.stringify(point)} className='circle' style={{ top: point.clientY, left: point.clientX, position: "absolute" }}></div>
+      ))}
     </div>
   );
 }
